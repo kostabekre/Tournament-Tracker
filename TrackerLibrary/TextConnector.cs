90b47f7205ca57c752a3b68a988dd8ee9142e24e
kostabekre <kostabekre@gmail.com>
2023-01-29 13:21:43 +0300

    created fake Text connector class.

---
+++ TrackerLibrary/TextConnector.cs
@@ -0,0 +1,14 @@
+﻿using TrackerLibrary.Model;
+
+namespace TrackerLibrary
+{
+    public class TextConnector : IDataConnection
+    {
+        // TODO: Wire up the method for text files.
+        public PrizeModel CreatePrize(PrizeModel model)
+        {
+            model.Id = 1;
+            return model;
+        }
+    }
+}
