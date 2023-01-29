41ad5b05c0d67067c4e11f0783f3c911aa84b4b1
kostabekre <kostabekre@gmail.com>
2023-01-29 14:18:31 +0300

    added new constructor to prize model

--- TrackerLibrary/Model/PrizeModel.cs
+++ TrackerLibrary/Model/PrizeModel.cs
@@ -19,5 +19,27 @@
         /// If PrizeAmount is not set, percentage can be used instead.
         /// </summary>
         public double PrizePercentage { get; set; }
+
+        public PrizeModel()
+        {
+
+        }
+
+        public PrizeModel(string placeName, string placeNumber, string prizeAmount, string prizePercentage)
+        {
+            PlaceName = placeName;
+
+            int placeNumberValue = 0;
+            int.TryParse(placeNumber, out placeNumberValue);
+            PlaceNumber = placeNumberValue;
+
+            decimal prizeAmountValue = 0;
+            decimal.TryParse(prizeAmount, out prizeAmountValue);
+            PrizeAmount = prizeAmountValue;
+
+            double prizePercentageValue = 0;
+            double.TryParse(prizePercentage, out prizePercentageValue);
+            PrizePercentage = prizePercentageValue;
+        }
     }
 }
