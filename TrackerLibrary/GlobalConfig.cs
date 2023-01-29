9cdf8e264da57cb491f815e14d51361c060518e7
kostabekre <kostabekre@gmail.com>
2023-01-29 13:30:34 +0300

    created sql and text connectors in global config, added  the library reference to the UI.

--- TrackerLibrary/GlobalConfig.cs
+++ TrackerLibrary/GlobalConfig.cs
@@ -2,20 +2,22 @@
 {
     public static class GlobalConfig
     {
-        public static List<IDataConnection> Connections { get; private set; }
+        public static List<IDataConnection> Connections { get; private set; } = new List<IDataConnection>();
 
         public static void InitializeConnections(bool database, bool textFiles)
         {
             if (database)
             {
-                // TODO: create the SQL connection
-                throw new NotImplementedException();
+                // TODO: set up the sql Connector properly
+                var sql = new SqlConnector();
+                Connections.Add(sql);
             }
 
             if (textFiles)
             {
-                // TODO: create the Text connection
-                throw new NotImplementedException();
+                // TODO: set up the text Connector properly
+                var text = new TextConnector();
+                Connections.Add(text);
             }
         }
     }
