26a0c5f6f910ea546de37a75889c20038f0fa605
kostabekre <kostabekre@gmail.com>
2023-01-29 13:09:26 +0300

    Created Global config without implementation.

---
+++ TrackerLibrary/GlobalConfig.cs
@@ -0,0 +1,22 @@
+﻿namespace TrackerLibrary
+{
+    public static class GlobalConfig
+    {
+        public static List<IDataConnection> Connections { get; private set; }
+
+        public static void InitializeConnections(bool database, bool textFiles)
+        {
+            if (database)
+            {
+                // TODO: create the SQL connection
+                throw new NotImplementedException();
+            }
+
+            if (textFiles)
+            {
+                // TODO: create the Text connection
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
